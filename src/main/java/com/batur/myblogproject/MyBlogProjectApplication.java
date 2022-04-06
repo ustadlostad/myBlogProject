99f067250b2f3df9ec5608e9de7fade1a8f96937
baturturkmen <baturturkmen@hotmail.com>
2022-04-06 12:20:46 +0300

    Initial commit

---
+++ src/main/java/com/batur/myblogproject/MyBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.batur.myblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/batur/myblogproject/controller/HomePageController.java
@@ -0,0 +1,17 @@
+package com.batur.myblogproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomePageController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePageController(){
+
+
+        return "homePage";
+    }
+
+}
